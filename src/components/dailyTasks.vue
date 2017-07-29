f1b914c2db8253bd5231f47bcb73b42354107950
arthan1011 <arthan1011@gmail.com>
2017-07-29 20:42:12 +0300

    new task style fix

--- src/components/dailyTasks.vue
+++ src/components/dailyTasks.vue
@@ -1,7 +1,26 @@
 <template>
     <div>
         <div class="ctrl-panel">
-            <input type="button" class="add-btn" value="Add" />
+            <div class="ctrl-wrapper">
+                <transition name="add-button" v-on:after-leave="afterAddButtonLeave">
+                    <input v-show="showAddButton" type="button" @click="showTaskInputs" class="add-btn" value="Add" />
+                </transition>
+                <transition name="formInputs" >
+                    <div class="add-inputs" v-show="showInputs">
+                        <transition>
+                            <div>
+                                <div class="form-input">
+                                    <label for="new-task-input-name">Name</label>
+                                    <input id="new-task-input-name" type="text" />
+                                </div>
+                                <div class="form-input">
+                                    <button class="submit-btn" type="button">Add</button>
+                                </div>
+                            </div>
+                        </transition>
+                    </div>
+                </transition>
+            </div>
         </div>
         <task-list :items="$store.state.dailyTasks"></task-list>
     </div>
@@ -18,12 +37,26 @@
         },
 
         data() {
-            return {}
+            return {
+                showInputs: false,
+                showAddButton: true
+            }
         },
 
         created() {
             console.log("Route component 'Daily Tasks' created!");
             this.$store.dispatch('loadDailyTasks');
+        },
+
+        methods: {
+            showTaskInputs() {
+                console.log("Trying to add a new task");
+                this.showAddButton = false;
+            },
+            afterAddButtonLeave() {
+                console.log("After add button leave");
+                this.showInputs = true;
+            }
         }
     }
 </script>
