4a9441bd69a3c8daa8a78f54ee460767f8a389cd
arthan1011 <arthan1011@gmail.com>
2017-08-02 21:26:57 +0300

    header style fix

--- src/components/appHeader.vue
+++ src/components/appHeader.vue
@@ -1,12 +1,14 @@
 <template>
     <header class="app-header">
-        <div class="left-btn-wrapper">
+        <div class="header-item-wrapper">
             <button v-bind:class="classObject" @click="addButtonClick">{{buttonLabel}}</button>
         </div>
         <nav class="links">
             <router-link class="r-link" to="/daily" tag="button">Daily</router-link><router-link class="r-link" to="/other" tag="button">Other</router-link>
         </nav>
-        <a class="logout" href="/logout"><span></span></a>
+        <div class="header-item-wrapper">
+            <a class="logout" href="/logout"><span></span></a>
+        </div>
     </header>
 </template>
 
