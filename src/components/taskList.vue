1d476089ec9a3d651cd481ea788fcbb794db8f7d
shamsiev <shamsiev@iqmen.ru>
2017-08-08 12:44:51 +0300

    daily tasks dateline

--- src/components/taskList.vue
+++ src/components/taskList.vue
@@ -2,7 +2,9 @@
     <div class="task-list">
         <div class="task-element">
             <div class="tl-date-label"></div>
-            <div class="tl-dates"></div>
+            <div class="tl-dates" v-for="item in datelineItems">
+                <div class="tl-dl-item" :title="`${item.day}.${item.month}.${item.year}`">{{item.day}}</div>
+            </div>
         </div>
         <transition-group name="taskList" tag="div">
             <div class="task-element" v-for="task in items" v-bind:key="task.name">
@@ -28,13 +30,29 @@
             items: Array
         },
         data() {
-            return {}
+            return {
+                datelineItems: createDateLine()
+            }
         },
 
         created() {
             console.log("Component Task list created!");
         },
 
+    }
+
+    function createDateLine() {
+        const result = [];
+        let now = new Date();
+        for (let i = 0; i < 21; i++) {
+            result.push({
+                day: now.getDate(),
+                month: now.getMonth() + 1,
+                year: now.getFullYear(),
+            });
+            now.setDate(now.getDate() + 1);
+        }
+        return result;
     }
 </script>
 
