b543fd2016817daa5c88365e1b6a25b58ceb6428
arthan1011 <arthan1011@gmail.com>
2017-08-11 21:53:10 +0300

    complete task modal component

---
+++ src/components/modal/completeTaskModal.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="complete-task-modal">
+        <div class="modal-mask" v-show="show">
+            <div class="modal-container">
+                <button @click="onCancel">Cancel</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'complete-task-modal',
+
+        data() {
+            return {}
+        },
+
+        props: {
+            show: Boolean
+        },
+
+        methods: {
+            onCancel() {
+                this.$emit('modal:cancel');
+            }
+        }
+    }
+</script>
+
+<style lang="sass">
+    @import "completeTaskModal.scss"
+</style>
--- src/components/taskList.vue
+++ src/components/taskList.vue
@@ -1,5 +1,12 @@
 <template>
     <div class="task-list">
+        <complete-task-modal :show="showModal"
+                             v-on:modal:cancel="showModal = false" />
+        <!--<div class="modal-mask" v-show="showModal">
+            <div class="modal-container">
+                <button @click="showModal = false">Cancel</button>
+            </div>
+        </div>-->
         <div class="task-element">
             <div class="tl-date-label"></div>
             <div class="tl-dates" v-for="item in tasksInfo.meta.dates">
@@ -13,6 +20,7 @@
                 </div>
                 <div class="tl-dateline">
                     <div class="tl-dl-item"
+                         @click="showCompleteTaskModal(task)"
                          v-bind:class="[item.complete ? 'done' : 'fail']"
                          v-for="item in task.datelineItems"></div>
                 </div>
@@ -23,34 +31,33 @@
 
 <script>
     import axios from 'axios';
+    import CompleteTaskModal from 'components/modal/completeTaskModal.vue';
 
     export default {
         name: 'task-list',
+        components: {
+            CompleteTaskModal
+        },
         props: {
             tasksInfo: Object
         },
         data() {
-            return {}
+            return {
+                showModal: false,
+            }
         },
 
         created() {
             console.log("Component Task list created!");
         },
 
-    }
-
-    function createDateLine() {
-        const result = [];
-        let now = new Date();
-        for (let i = 0; i < 21; i++) {
-            result.push({
-                day: now.getDate(),
-                month: now.getMonth() + 1,
-                year: now.getFullYear(),
-            });
-            now.setDate(now.getDate() + 1);
+        methods: {
+            showCompleteTaskModal(task) {
+                console.log(`Showing complete task ${task.name} modal `);
+                this.showModal = true;
+            }
         }
-        return result;
+
     }
 </script>
 
