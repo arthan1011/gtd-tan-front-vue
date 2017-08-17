1c1c8c03ea0d6a92091b2ac35c5ea1298c1dab10
arthan1011 <arthan1011@gmail.com>
2017-08-17 21:23:27 +0300

    Revert "modal enter-leave animation fix"
    This reverts commit 16d6f626739bde3336ec19886bcf4f288aac7819.

--- src/components/modal/completeTaskModal.vue
+++ src/components/modal/completeTaskModal.vue
@@ -9,10 +9,24 @@
                         </div>
                     </div>
                     <div class="modal-body">
-                        <manual-confirmation :show="show"
-                                             v-on:task:done="emitTaskCompleted"
-                                             :taskDate="taskDate"></manual-confirmation>
-
+                        <div class="manual-confirmation">
+                            <div class="confirm-main">
+                                <div class="block-wrapper">
+                                    <div class="moving-block"
+                                         @transitionend="movingBlockTransitionEnd"
+                                         :class="movingBlockClass">
+                                    </div>
+                                </div>
+                                <div class="moving-gear"
+                                     :class="movingBlockClass">
+                                </div>
+                            </div>
+                            <button class="confirm-press-button"
+                                    @mouseup="pressButtonUp"
+                                    @mouseleave="pressButtonUp"
+                                    @mousedown="pressButtonDown"
+                            >Press to complete<br> {{taskName}}</button>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -21,18 +35,44 @@
 </template>
 
 <script>
-    //TODO: Переделать в универсальное модальное окно с анимацией появления
-    import ManualConfirmation from 'components/modal/manualConfirmation.vue';
-
     export default {
         name: 'complete-task-modal',
-        components: {
-            ManualConfirmation
+
+        data() {
+            return {
+                movingBlockClass: {
+                    "anime-rot-1": false,
+                    "anime-rot-2": false,
+                    "anime-rot-3": false,
+                    "anime-rot-end": false,
+                    "anime-rot-back": false,
+                }
+            }
         },
 
         props: {
             show: Boolean,
-            taskDate: Object,
+            taskDate: Object
+        },
+
+        computed: {
+            taskName() {
+                return this.$store.state.daily.tasks.find((t) => t.id === this.taskDate.id).name;
+            }
+        },
+
+        watch: {
+            show(isShown) {
+                if (isShown) {
+                    this.movingBlockClass = {
+                        "anime-rot-1": false,
+                        "anime-rot-2": false,
+                        "anime-rot-3": false,
+                        "anime-rot-end": false,
+                        "anime-rot-back": false,
+                    };
+                }
+            }
         },
 
         methods: {
@@ -42,6 +82,40 @@
             emitTaskCompleted() {
                 this.$emit('task:done');
             },
+            pressButtonDown() {
+                if (!this.movingBlockClass["anime-rot-end"]) {
+                    this.movingBlockClass["anime-rot-1"] = true;
+                }
+            },
+            pressButtonUp() {
+                this.movingBlockClass["anime-rot-back"] = false;
+                this.movingBlockClass["anime-rot-1"] = false;
+                this.movingBlockClass["anime-rot-2"] = false;
+                this.movingBlockClass["anime-rot-3"] = false;
+                if (!this.movingBlockClass["anime-rot-end"]) {
+                    this.movingBlockClass["anime-rot-back"] = true;
+                }
+            },
+            movingBlockTransitionEnd() {
+                this.movingBlockClass["anime-rot-back"] = false;
+
+                if (this.movingBlockClass["anime-rot-1"]) {
+                    this.movingBlockClass["anime-rot-1"] = false;
+                    this.movingBlockClass["anime-rot-2"] = true;
+                    return;
+                }
+                if (this.movingBlockClass["anime-rot-2"]) {
+                    this.movingBlockClass["anime-rot-2"] = false;
+                    this.movingBlockClass["anime-rot-3"] = true;
+                    return;
+                }
+                if (this.movingBlockClass["anime-rot-3"]) {
+                    this.movingBlockClass["anime-rot-3"] = false;
+                    this.movingBlockClass["anime-rot-end"] = true;
+                    this.emitTaskCompleted()
+                }
+
+            }
         }
     }
 </script>
