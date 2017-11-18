06d80220ac522e3a189adb1e502d897d2f6c8f45
arthan1011 <arthan1011@gmail.com>
2017-11-18 22:20:13 +0300

    gtd-tan-front-vue-13 User authorization and registration. Dev server auth

--- bin/dev-www.js
+++ bin/dev-www.js
@@ -6,15 +6,16 @@ const PORT = 4200;
 const express = require('express');
 const app = express();
 const webpack = require('webpack');
+const handlebars = require('express-handlebars');
 const webpackDevMiddleware = require('webpack-dev-middleware');
 const webpackHotMiddleware = require("webpack-hot-middleware");
 const config = require('../webpack.config.js');
+const setupAuthentication = require('../lib/auth');
 
 app.set('port', process.env.PORT || PORT);
+setupAuthentication(app);
 
 const compiler = webpack(config);
-// app.use('/', express.static('dist'));
-
 app.use(webpackDevMiddleware(compiler, {
     publicPath: config.output.publicPath
 }));
@@ -24,6 +25,14 @@ app.get('/hello', (req, res) => {
     res.send('Hello, expressJs!')
 });
 
+// Template
+app.engine('handlebars', handlebars({
+    defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+app.get('/login', (req, res) => {
+    res.render('login');
+});
 
 app.listen(app.get('port'), () => {
     console.log(`Gtd-tan front end started at port ${app.get('port')}`);
